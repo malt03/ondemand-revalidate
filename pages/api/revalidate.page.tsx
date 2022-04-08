3c3424513afd5eff10eda992ab3a60201f07a2fd
Koji Murata <malt.koji@gmail.com>
2022-04-08 10:23:23 +0900

    revalidatepath

--- pages/api/revalidate.page.tsx
+++ pages/api/revalidate.page.tsx
@@ -1,7 +1,7 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-  const id = req.query["id"];
-  await res.unstable_revalidate(`/_time/${id}`);
-  res.status(200).json({ revalidate: id });
+  const path = req.query["path"] as string;
+  await res.unstable_revalidate(path);
+  res.status(200).json({ revalidate: path });
 };

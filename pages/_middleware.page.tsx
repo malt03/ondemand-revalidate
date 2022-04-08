798a6c65678ee18a09a5e6334f3c7162b6bb66f1
Koji Murata <malt.koji@gmail.com>
2022-04-08 10:39:52 +0900

    戻してログ

--- pages/_middleware.page.tsx
+++ pages/_middleware.page.tsx
@@ -1,24 +1,25 @@
 import { NextMiddleware, NextResponse } from "next/server";
 
 const middleware: NextMiddleware = (req) => {
-  return NextResponse.next();
-  // if (req.headers.get("x-prerender-revalidate") != undefined) return NextResponse.next();
+  console.info(req.nextUrl.pathname);
 
-  // if (req.nextUrl.pathname.startsWith(`/api`)) return NextResponse.next();
-  // if (req.nextUrl.pathname.startsWith("/site")) return NextResponse.next();
-  
-  // const hostname = req.headers.get("host");
-  // if (hostname == undefined) {
-  //   return new Response(null, {
-  //     status: 400,
-  //     statusText: "No hostname found in request headers",
-  //   });
-  // }
+  if (req.headers.get("x-prerender-revalidate") != undefined) return NextResponse.next();
 
-  // const url = req.nextUrl.clone();
-  // url.pathname = `/site/${hostname}${url.pathname}`;
+  if (req.nextUrl.pathname.startsWith(`/api`)) return NextResponse.next();
+  if (req.nextUrl.pathname.startsWith("/site")) return NextResponse.next();
   
-  // return NextResponse.rewrite(url);
+  const hostname = req.headers.get("host");
+  if (hostname == undefined) {
+    return new Response(null, {
+      status: 400,
+      statusText: "No hostname found in request headers",
+    });
+  }
+
+  const url = req.nextUrl.clone();
+  url.pathname = `/site/${hostname}${url.pathname}`;
+  
+  return NextResponse.rewrite(url);
 };
 
 export default middleware;

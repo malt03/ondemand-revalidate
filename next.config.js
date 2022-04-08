c1c1c2ec26d8cdd4bb2cf44119e4a4d8090dff25
Koji Murata <malt.koji@gmail.com>
2022-04-08 10:49:56 +0900

    i18n削除

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
 const nextConfig = {
-  i18n: {
-    locales: ["ja", "en-US"],
-    defaultLocale: "ja",
-    localeDetection: false,
-  },
+  // i18n: {
+  //   locales: ["ja", "en-US"],
+  //   defaultLocale: "ja",
+  //   localeDetection: false,
+  // },
   pageExtensions: ["page.tsx", "api.ts"],
 }
 
